bcfd641fa6927b0a2f9a054a317f8c4f6581f9c9
Stephen Valdinger <stephen@chocolatey.io>
2025-08-26 20:34:27 -0400

    (1.8.0) Add ability to supply a scriptblock for  a definition.
    With this change, you can supply a scriptblock and it will be saved as Definition.ps1 in the configuration path.

--- source/public/New-LabConfiguration.ps1
+++ source/public/New-LabConfiguration.ps1
@@ -18,13 +18,16 @@ function New-LabConfiguration {
     .PARAMETER Url
     A url to a PowerShell script you wish to include as the definition
     
+    .PARAMETER ScriptBlock
+    A PowerShell script block that will be saved as the definition
+    
     .EXAMPLE
     $conf = @{
-    Name = 'Example'
-    Definition = 'C:\temp\sample.ps1'
-    Parameters = @{
-        Animal = 'Dog'
-        Breed = 'Lab'
+        Name = 'MyDomainLab'
+        Definition = 'C:\Labs\DomainController.ps1'
+        Parameters = @{
+            DomainName = 'contoso.com'
+            AdminPassword = 'P@ssw0rd123!'
         }
     }
 
@@ -32,15 +35,26 @@ function New-LabConfiguration {
     
     .EXAMPLE
     $conf = @{
-    Name = 'Example'
-    Url = 'https://files.fabrikam.com/myscript.ps1'
-    Parameters = @{
-        Animal = 'Dog'
-        Breed = 'Lab'
-       }
+        Name = 'SQLServerLab'
+        Url = 'https://raw.githubusercontent.com/AutomatedLab/AutomatedLab/main/LabSources/SampleScripts/Introduction/03%20SQL%20Server%20and%20client,%20domain%20joined.ps1'
+        Parameters = @{
+            SQLServiceAccount = 'CONTOSO\SQLService'
+            DatabaseName = 'ProductionDB'
+        }
     }
 
     New-LabConfiguration @conf
+
+    .EXAMPLE
+    $scriptBlock = {
+        New-LabDefinition -Name $Parameters.LabName -DefaultVirtualizationEngine HyperV
+        Add-LabDomainDefinition -Name contoso.com -AdminUser Install -AdminPassword P@ssw0rd123!
+        Add-LabMachineDefinition -Name DC01 -Memory 2GB -Roles RootDC -DomainName contoso.com
+        Add-LabMachineDefinition -Name Client01 -Memory 1GB -OperatingSystem 'Windows 10 Enterprise' -DomainName contoso.com
+        Install-Lab
+    }
+
+    New-LabConfiguration -Name 'BasicDomainLab' -ScriptBlock $scriptBlock -Parameters @{ LabName = 'TestDomain' }
 
 
     #>
@@ -48,6 +62,7 @@ function New-LabConfiguration {
     Param(
         [Parameter(Mandatory, ParameterSetName = 'default')]
         [Parameter(Mandatory, ParameterSetName = 'Git')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptBlock')]
         [String]
         $Name,
 
@@ -57,12 +72,17 @@ function New-LabConfiguration {
 
         [Parameter(ParameterSetName = 'Git')]
         [Parameter(ParameterSetName = 'default')]
+        [Parameter(ParameterSetName = 'ScriptBlock')]
         [Hashtable]
         $Parameters,
 
         [Parameter(Mandatory, ParameterSetName = 'Git')]
         [String]
-        $Url
+        $Url,
+
+        [Parameter(Mandatory, ParameterSetName = 'ScriptBlock')]
+        [ScriptBlock]
+        $ScriptBlock
     )
 
     end {
@@ -83,6 +103,9 @@ function New-LabConfiguration {
             'Git' {
                 $Definition = Join-Path $Configuration -ChildPath 'Definition.ps1' 
             }
+            'ScriptBlock' {
+                $Definition = Join-Path $Configuration -ChildPath 'Definition.ps1'
+            }
             default {
                 $Definition = Resolve-Path $Definition
             }
@@ -94,11 +117,15 @@ function New-LabConfiguration {
         } | Export-Configuration -CompanyName $env:USERNAME -Name $Name -Scope User
 
         # The configuration has to exist on disk before we can use it to build the path
-        # where the definition will be saved when downloading from a Url.
+        # where the definition will be saved when downloading from a Url or saving a ScriptBlock.
         # So we postpone processing until we have exported the configuration with the correct
         # value, and then just drop the file there.
         if ($url) {
             [System.Net.WebClient]::new().DownloadFile($Url, $Definition)       
+        }
+        
+        if ($ScriptBlock) {
+            $ScriptBlock.ToString() | Out-File -FilePath $Definition -Encoding UTF8
         }         
 
     }
